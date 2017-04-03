a6b6213bfa484df8adc389b958d6f467dd7efed6
thethego <theo.goria@etu.univ-lyon1.fr>
2017-04-03 10:40:47 +0200

    project creation

---
+++ src/client/Main.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by theo on 03/04/17.
+ */
+public class Main {
+}
---
+++ src/serveur/Main.java
@@ -0,0 +1,7 @@
+package serveur;
+
+/**
+ * Created by theo on 03/04/17.
+ */
+public class Main {
+}
